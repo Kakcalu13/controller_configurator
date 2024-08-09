bcdcbc5f35f345a255096aff18976bc7be0e7272
kakcalu13 <kevin.a.araujo@gmail.com>
2024-08-09 00:58:37 -0400

    added simple godot

---
+++ configurator_example/main.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+var capabilities
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	capabilities = load_capabilities()
+	var json = JSON.new()
+	var error = json.parse(capabilities)
+	if error == OK:
+		var capabilities = json.data
+	else:
+		print("JSON Parse Error: ", json.get_error_message(), " in ", capabilities, " at line ", json.get_error_line())
+
+	
+
+func load_capabilities():
+	var file = FileAccess.open("res://configuration.json", FileAccess.READ)
+	var content = file.get_as_text()
+	return content
